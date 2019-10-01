4be31f7cd276af652169347b314c4a06bbf89bd4
raphaelkhan8 <raphaelkhan8@gmail.com>
2019-10-01 16:34:14 -0500

    fixed linting error

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Contorl-Allow-Methods', 'OPTIONS, GET, POST, PUT, DELETE');
-  if ('OPTIONS' == req.method) {
+  if (req.method === 'OPTIONS') {
     res.sendStatus(200);
   } else {
     console.log(`${req.ip} ${req.method} ${req.url}`);
@@ -21,6 +21,6 @@ app.get('/', (req, res) => {
 
 const PORT = 4201;
 
-app.listen(PORT, '127.0.0.1', function () {
+app.listen(PORT, '127.0.0.1', () => {
   console.log(`Server listening on port ${PORT}`);
 });
