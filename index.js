df1229f80b0c6bec5ef3132bc023f57cc60f822f
sdelafuente12 <s.delafuente831@gmail.com>
2019-10-10 15:26:30 -0500

    refactored getAllUsersTrips

--- index.js
+++ index.js
@@ -161,20 +161,23 @@ app.get('/getAllUsersTrips', (req, res) => {
     .then((tripArray) => {
       tripArray.map((trip) => {
         const currently = new Date();
-        // console.log('@@@ DATE START @@@', trip[0].dataValues.dateStart);
-        // console.log('@@@ DATE END @@@', trip[0].dataValues.dateEnd);
-        if (trip.dateEnd < currently) {
-          console.log('this trip has ended');
-        } if (trip.dateStart > currently) {
-          console.log('this trip is in the future');
+        if (trip[0].dataValues.dateStart < currently && trip[0].dataValues.dateEnd > currently) {
+          trip[0].dataValues.status = 'current';
+          console.log(trip[0].dataValues.route);
+        } else if (trip[0].dataValues.dateStart > currently) {
+          trip[0].dataValues.status = 'upcoming';
+          console.log(trip[0].dataValues.route);
+        } else if (trip[0].dataValues.dateEnd < currently) {
+          trip[0].dataValues.status = 'previous';
+          console.log(trip[0].dataValues.route);
         }
-        console.log('youre on this trip');
       });
+      res.send(tripArray);
     })
-    .then((response) => {
-      // console.log(response);
-      res.send(response);
-    })
+    // .then((response) => {
+    //   console.log(response);
+    //   res.send(response);
+    // })
     .catch((err) => {
       // console.log('Err trying to get user trips from the database', err);
       res.status(400).send(err);
