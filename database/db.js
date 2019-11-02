972ccf70dfd72af99db051f80f2fd2e00adf8799
raphaelkhan8 <40365951+raphaelkhan8@users.noreply.github.com>
2019-11-01 21:45:47 -0500

    add  database_url env

--- database/db.js
+++ database/db.js
@@ -1,18 +1,22 @@
 const Sequelize = require('sequelize');
 
 const {
-  DATABASE,
-  USER_NAME,
-  USER_PASSWORD,
-  HOST,
-  PORT,
+  DATABASE_URL,
+  // USER_NAME,
+  // USER_PASSWORD,
+  // HOST,
+  // PORT,
 } = process.env;
 
-module.exports = new Sequelize(DATABASE, USER_NAME, USER_PASSWORD,
+module.exports = new Sequelize(DATABASE_URL);
+
+/*
+DATABASE, USER_NAME, USER_PASSWORD,
   {
     host: HOST,
     port: PORT,
     dialect: 'postgres',
-  });
+  }
+  */
 
 // module.exports = new Sequelize('postgres://postgres:postgres@localhost:5432/your-next-stop');
