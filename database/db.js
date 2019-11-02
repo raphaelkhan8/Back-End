66ab225d36a51ed9f9d3ac83ae909c056ba4ee1b
raphaelkhan8 <40365951+raphaelkhan8@users.noreply.github.com>
2019-11-01 22:45:32 -0500

    remove unused code

--- database/db.js
+++ database/db.js
@@ -1,22 +1,3 @@
 const Sequelize = require('sequelize');
 
-const {
-  DATABASE_URL,
-  // USER_NAME,
-  // USER_PASSWORD,
-  // HOST,
-  // PORT,
-} = process.env;
-
-module.exports = new Sequelize(DATABASE_URL);
-
-/*
-DATABASE, USER_NAME, USER_PASSWORD,
-  {
-    host: HOST,
-    port: PORT,
-    dialect: 'postgres',
-  }
-  */
-
-// module.exports = new Sequelize('postgres://postgres:postgres@localhost:5432/your-next-stop');
+module.exports = new Sequelize(process.env.DATABASE_URL);
